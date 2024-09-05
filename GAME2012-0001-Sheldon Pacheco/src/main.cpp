03e6584fa844b0c49202dd366e65504df80018b9
Sheldon Pacheco <sheldon.p@outlook.com>
2024-09-04 22:46:32 -0400

    c

--- GAME2012-0001-Sheldon Pacheco/src/main.cpp
+++ GAME2012-0001-Sheldon Pacheco/src/main.cpp
@@ -1,8 +1,15 @@
 #include "GLFW/glfw3.h"  
 #include <glad/glad.h>
 #include "iostream"
+#include "random"
+#include "cstdlib"
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 720;
+float Random(float min, float max)
+{
+    return min + (rand() / ((float)RAND_MAX / (max - min)));
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 int main(void)
 {
@@ -32,6 +39,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.22f, 0.49f, 0.17f, 1.0f);
+        float x = Random(0.0f, SCREEN_WIDTH);
+        float y = Random(0.0f, SCREEN_HEIGHT);
+        glfwSetCursorPos(window, x, y);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
